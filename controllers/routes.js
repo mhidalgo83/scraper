db51254ba5436b097fbde7d5a4d8ea77aab892c9
Matt <makeshifthidalgo@hotmail.com>
2018-08-27 16:35:12 -0700

    pulling data from scrape, need to get links

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,11 +1,29 @@
 var db = require("../models");
 var cheerio = require("cheerio");
 var request = require("request");
+var path = require("path");
 
 module.exports = function (app) {
 
+    //Route for index.html...
     app.get("/", function (req, res) {
-        res.render("index");
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    //Route for scrape
+    app.get("/articles", function (req, res) {
+        request("https://www.fantasyflightgames.com/en/index/", function(err, res, html){
+            var $ = cheerio.load(html);
+            
+            $("span a").each(function(i, element){
+                var result = {};
+                result.title = $(this).text();
+                console.log(result);
+                result.link = $(this).attr("href");
+            })
+            
+        })
+        res.render("articles");
     });
 
 }
